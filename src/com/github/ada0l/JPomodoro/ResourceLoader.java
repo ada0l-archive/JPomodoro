567e3f28b106e7450e221ff0936d07c08b8463c0
ada0l <andreika.varfolomeev@yandex.ru>
2020-07-28 10:29:59 +1000

    added ResourceLoader.loadSound

--- src/com/github/ada0l/JPomodoro/ResourceLoader.java
+++ src/com/github/ada0l/JPomodoro/ResourceLoader.java
@@ -1,17 +1,38 @@
 package com.github.ada0l.JPomodoro;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
+import java.io.InputStream;
 import java.util.Objects;
 
 public class ResourceLoader {
 
     public static final String PATH_IMAGE = "images/";
+    public static final String PATH_SOUND = "sounds/";
+    private final ClassLoader cl = this.getClass().getClassLoader();
 
     public Image loadImage(String filename) {
-
-        ClassLoader cl = this.getClass().getClassLoader();
-
         return new ImageIcon(Objects.requireNonNull(cl.getResource(PATH_IMAGE + filename))).getImage();
+    }
+
+    public Clip loadSound(String filename) {
+        Clip clip = null;
+        try {
+             clip = AudioSystem.getClip();
+             InputStream inputStream = cl.getResourceAsStream(
+                     PATH_SOUND + filename
+             );
+            assert inputStream != null;
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+             System.out.println(clip.isOpen());
+             clip.open(audioInputStream);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return clip;
     }
 }
